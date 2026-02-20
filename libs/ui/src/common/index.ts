c9abfa44cd8e04135411b310b60f477db6a65520
Ker1n <Ker1n.mkc@gmail.com>
2026-02-21 00:33:02 +0100

    mixins and change variables

--- libs/ui/src/common/index.ts
+++ libs/ui/src/common/index.ts
@@ -2,3 +2,4 @@ export {ButtonComponent} from './button/button';
 export {InputComponent} from './input/input';
 export {CardComponent} from './card/card';
 export {DividerComponent} from './divider/divider';
+export {LoaderComponent} from './loader/loader';
