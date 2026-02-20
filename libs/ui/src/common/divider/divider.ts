46fca20162e187ae177058b381c05abf45a67c5e
Ker1n <Ker1n.mkc@gmail.com>
2026-02-20 23:38:11 +0100

    divider component

---
+++ libs/ui/src/common/divider/divider.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'bw-divider',
+  templateUrl: './divider.html',
+  styleUrl: './divider.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DividerComponent {
+
+}
