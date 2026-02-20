5cacccdd64f71a85929f59ce8574d3979e002c45
Ker1n <Ker1n.mkc@gmail.com>
2026-02-20 23:24:49 +0100

    card component

---
+++ libs/ui/src/common/card/card.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bw-card',
+  standalone: true,
+  templateUrl: './card.html',
+  styleUrls: ['./card.scss'],
+})
+export class Card {}
+
