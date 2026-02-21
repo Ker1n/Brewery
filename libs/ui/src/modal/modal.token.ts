f48d974f96914dd78669e2da718245afd81dee7d
Ker1n <Ker1n.mkc@gmail.com>
2026-02-21 01:34:27 +0100

    fix modal type token

--- libs/ui/src/modal/modal.token.ts
+++ libs/ui/src/modal/modal.token.ts
@@ -1,3 +1,3 @@
 import {InjectionToken} from '@angular/core';
 
-export const MODAL_DATA = new InjectionToken<unknown>('MODAL_DATA');
+export const MODAL_DATA = new InjectionToken<any>('MODAL_DATA');
